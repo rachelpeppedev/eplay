cf968488a0595674fbc9bfe0fbe056e132dc0336
Rachel Peppe <rapeppe@hotmail.com>
2024-09-12 15:18:05 -0300

    Componentizando Parte I

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 import Banner from './components/Banner'
 import Header from './components/Header'
 import { GlobalCss } from './styles'
+
+const rotas = createBrowserRouter([
+  {
+    path: '/',
+    element: <Banner />
+  }
+])
 function App() {
   return (
     <>
@@ -8,7 +17,7 @@ function App() {
       <div className="container">
         <Header />
       </div>
-      <Banner />
+      <RouterProvider router={rotas} />
     </>
   )
 }
