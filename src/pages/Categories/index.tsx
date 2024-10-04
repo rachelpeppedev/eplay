82dc80b2fb9a06a0ddba5eb9d5ec0a0785d1b43d
Rachel Peppe <rapeppe@hotmail.com>
2024-10-04 12:09:53 -0300

    Fazendo a responsividade

--- src/pages/Categories/index.tsx
+++ src/pages/Categories/index.tsx
@@ -25,14 +25,30 @@ const Categories = () => {
   if (actionGames && fightGames && rpgGames && sportGames && simulationGames) {
     return (
       <>
-        <ProductsList games={actionGames} title="Ação" background="black" />
-        <ProductsList games={sportGames} title="Esportes" background="gray" />
-        <ProductsList games={fightGames} title="Luta" background="black" />
-        <ProductsList games={rpgGames} title="RPG" background="gray" />
+        <ProductsList
+          games={actionGames}
+          title="Ação"
+          background="black"
+          id="action"
+        />
+        <ProductsList
+          games={sportGames}
+          title="Esportes"
+          background="gray"
+          id="sports"
+        />
+        <ProductsList
+          games={fightGames}
+          title="Luta"
+          background="black"
+          id="fight"
+        />
+        <ProductsList games={rpgGames} title="RPG" background="gray" id="rpg" />
         <ProductsList
           games={simulationGames}
           title="Simulação"
           background="black"
+          id="simulation"
         />
       </>
     )
